06efeca9bc22b6779839ea796496d90c38baa0bd
rheber <rheber@rediffmail.com>
2016-08-08 18:51:51 +1000

    finish parser

--- hth.hs
+++ hth.hs
@@ -1,15 +1,23 @@
 import Text.Parsec
 import Text.Parsec.Char
 
+type Parser a = Parsec String () a
+data TimePeriod = AnyTime | Week | Month deriving Show
+
 data Command =
   Delete Int |
+  Help |
+  List TimePeriod |
   Mark Int |
   Monthly String |
+  QuitUnsafe |
   Quit |
+  Rename Int String |
+  Renumber |
+  Retime Int |
+  Save |
   Weekly String
   deriving Show
-
-type Parser a = Parsec String () a
 
 integer :: Parser Int
 integer = fmap read $ many1 digit
@@ -17,8 +25,20 @@ integer = fmap read $ many1 digit
 taskName :: Parser String
 taskName = many1 anyChar
 
+timePeriod :: Parser TimePeriod
+timePeriod =
+  string "all" *> pure AnyTime <|>
+  string "monthly" *> pure Month <|>
+  string "weekly" *> pure Week
+
 delete :: Parser Command
 delete = Delete <$> (string "delete " *> integer)
+
+help :: Parser Command
+help = string "help" *> pure Help
+
+list :: Parser Command
+list = List <$> (string "list " *> timePeriod)
 
 mark :: Parser Command
 mark = Mark <$> (string "mark " *> integer)
@@ -26,23 +46,44 @@ mark = Mark <$> (string "mark " *> integer)
 monthly :: Parser Command
 monthly = Monthly <$> (string "monthly " *> taskName)
 
+quitUnsafe :: Parser Command
+quitUnsafe = string "quit!" *> pure QuitUnsafe
+
 quit :: Parser Command
-quit = do
-  string "quit"
-  return Quit
+quit = string "quit" *> pure Quit
+
+rename :: Parser Command
+rename = Rename <$> (string "rename " *> integer) <* space <*> taskName
+
+renumber :: Parser Command
+renumber = string "renumber" *> pure Renumber
+
+retime :: Parser Command
+retime = Retime <$> (string "retime " *> integer)
+
+save :: Parser Command
+save = string "save" *> pure Save
 
 weekly :: Parser Command
 weekly = Weekly <$> (string "weekly " *> taskName)
 
 command :: Parser Command
-command = delete <|>
+command =
+  delete <|>
+  help <|>
+  list <|>
   try mark <|>
   monthly <|>
+  try quitUnsafe <|>
   quit <|>
+  try rename <|>
+  try renumber <|>
+  retime <|>
+  save <|>
   weekly
 
 parseExpr :: String -> Command
 parseExpr input =
   case parse command "stdin" input of
-    Left err -> error (show err)
+    Left err -> error "Unrecgonised/incomplete command"
     Right expr -> expr
