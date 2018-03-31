f870be2e6778d968876783c5755e87cf20332aa6
rheber <rheber@rediffmail.com>
2018-03-31 15:48:36 +1100

    Change: Now look for hth.json anywhere on Path.

--- Config.hs
+++ Config.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE DeriveGeneric #-}
-
 module Config where
 
 import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy as BS
+import Data.List.Split (splitOn)
 import GHC.Generics
-import System.Directory (doesFileExist)
+import System.Directory (findFile)
+import System.Environment (lookupEnv)
 
 import State (HTHState(..))
 
@@ -17,21 +18,38 @@ instance Aeson.FromJSON Config
 
 defaultConfig = Config{habitsFilename = "habits", habitsFolder = "."}
 
+-- Read config from file.
 configRead :: String -> IO Config
 configRead filename = do
   cfgString <- BS.readFile filename
   return $ maybe defaultConfig id $ Aeson.decode cfgString
 
-makeConfig :: String -> IO Config
-makeConfig filename = do
-  cfgFileExists <- doesFileExist filename
-  if cfgFileExists
-  then configRead filename
-  else return defaultConfig
+-- Look for config file in current directory or Path.
+findConfig :: IO (Maybe String)
+findConfig = do
+  path <- lookupEnv "PATH"
+  case path of
+    Nothing -> searchPaths "."
+    (Just folders) -> searchPaths $ ".;" ++ folders
 
+-- Helper function for findConfig.
+searchPaths :: String -> IO (Maybe String)
+searchPaths path = do
+  let folders = splitOn ";" path -- Not tested on Unix.
+  findFile folders "hth.json"
+
+-- Make config, using file if found in current directory or Path.
+makeConfig :: IO Config
+makeConfig = do
+  file <- findConfig
+  case file of
+    Nothing -> return defaultConfig
+    (Just filename) -> configRead filename
+
+-- Add config to state.
 configureState :: HTHState -> IO HTHState
 configureState st = do
-  cfg <- makeConfig "hth.json"
+  cfg <- makeConfig
   let filename = habitsFilename cfg
   let folder = habitsFolder cfg
   return st{configFilename = filename, configFolder = folder}
